60de2d2cd5f3cdd47b33fa1926ba1be1cf5e1cfc
boulen_b <amin.boulenoir@epitech.eu>
2019-11-16 14:25:38 +0100

    Login actions on login page

--- app/src/pages/Login.vue
+++ app/src/pages/Login.vue
@@ -2,6 +2,30 @@
   <Page class="page">
     <ActionBar title="Login" class="action-bar" />
     <StackLayout>
+      <InputWithError
+        v-model="email"
+        hint="Email"
+        keyboardType="email"
+        autocorrect="false"
+        autocapitalizationType="none"
+        errorMessage="Invalid email format"
+        :error="emailError"
+      />
+      <InputWithError
+        v-model="password"
+        errorMessage="Password must contain digits, symbols, lowercase and uppercase letters and be at least 8 characters long"
+        :error="passwordError"
+        hint="Password"
+        autocorrect="false"
+        autocapitalizationType="none"
+        secure
+      />
+      <Button
+        :isEnabled="!loading"
+        text="Login"
+        class="btn btn-primary btn-rounded-sm"
+        @tap="login"
+      />
       <Button text="Or Register" @tap="goToRegister"></Button>
     </StackLayout>
   </Page>
@@ -9,11 +33,31 @@
 
 <script>
 import Register from "./Register.vue";
+import InputWithError from "../components/InputWithError.vue";
+import { validateEmail, validatePassword } from "../../utils/validator";
+
+import { mapActions } from "vuex";
+
 export default {
+  components: { InputWithError },
   data() {
     return {};
   },
+  ...mapActions("user", ["Login"]),
   methods: {
+    async login() {
+      this.emailError = !validateEmail(this.email);
+      this.passwordError = !validatePassword(this.password);
+      if (this.emailError || this.nameError || this.passwordError) {
+        return;
+      }
+      this.loading = true;
+      await this.Login({
+        email: this.email,
+        password: this.password
+      });
+      this.loading = false;
+    },
     goToRegister() {
       this.$navigateTo(Register);
     }
@@ -21,4 +65,14 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.input::placeholder {
+  color: white;
+  background-color: black;
+}
+
+.input {
+  background-color: black;
+  color: white;
+}
+</style>
